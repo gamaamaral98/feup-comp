05aeef05a5b02be1e6de3cf9983ca1eae9014119
Amadeu Pereira <up201605646@fe.up.pt>
2019-03-21 19:38:51 +0000

    operator precedence

--- java--/test.java
+++ java--/test.java
@@ -4,12 +4,14 @@ class Fac {
         if (num < 1)
             num_aux = 1;
         else
-            num_aux = num * (this.ComputeFac(num-1));
+            num_aux = num * false.ComputeFac(num-1).a().length;
+            num_aux = 1+4*2 && !2 + 2;
+            num_aux = 0;
         return num_aux;
     }
 
-    public static void main(String[] a){
-        io.println(new Fac().ComputeFac(10)); //assuming the existence
-                                              // of the classfile io.class
-    }
+    // public static void main(String[] a){
+    //     io.println(new Fac().ComputeFac(10)); //assuming the existence
+    //                                           // of the classfile io.class
+    // }
 }
