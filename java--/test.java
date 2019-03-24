fff921400b78ef336d88c2533a6f96fc6eb7df94
Nuno Lopes <up201605337@fe.up.pt>
2019-03-24 17:12:28 +0000

    Added more erros handling

--- java--/test.java
+++ java--/test.java
@@ -1,19 +1,30 @@
-class Fac {
+class Fac extends a b{
     int a;
-    public int ComputeFac(int num, boolean t){
-        int num_aux ;
-        if (num < 1) {
-            num_aux = 1;
-        }
-        else
-            num_aux = num * false.ComputeFac(num-1)[12 + 1].a().length;
-            num_aux = 1+4*2 && !2 + 2;
-            num_aux = 0;
-            a = new ABC();
-        return num_aux;
-    }
-
     public static void main(String[] a){
         io.println(new Fac().ComputeFac(10)); 
     }
+
+    public int ComputeFac(int num, boolean t){
+        banaa num_aux ;
+        if (num < 1) {
+            num_aux = 1;
+        }
+        else{
+            num_aux = num * false.ComputeFac(num-1)[12 + 1].a().length;
+        }
+
+        while(a = 0) {
+            b = 3;
+        }
+
+        num_aux = 1+4*2 && !2 + 2;
+        num_aux = 0;
+        a = new ABC();
+
+        return num_aux;
+    }
+
+    // public static void main(String[] a){
+    //     io.println(new Fac().ComputeFac(10)); 
+    // }
 }
