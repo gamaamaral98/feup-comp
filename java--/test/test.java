5380667622df846cd8ca855d7ed1145cfbcfd3b9
Amadeu Pereira <up201605646@fe.up.pt>
2019-04-29 23:34:08 +0100

    test

---
+++ java--/test/test.java
@@ -0,0 +1,14 @@
+class Fac {
+    public int ComputeFac(int num){
+        // if (num < 1)
+        //     num_aux = 1;
+        // else
+        //     num_aux = num * (this.ComputeFac(num-1));
+
+        return 1+2;
+    }
+
+    public static void main(String[] a){
+        io.println(new Fac().ComputeFac(10));
+    }
+}
