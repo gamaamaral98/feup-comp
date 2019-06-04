44805e9e0673f5b11230f637d632999953b4065d
Amadeu Pereira <up201605646@fe.up.pt>
2019-06-04 18:41:50 +0100

    fixed test.java

--- java--/test/Test.java
+++ java--/test/Test.java
@@ -1,8 +1,12 @@
 class Test {
     int[] a;
     public static void main(String[] args) {
+        new Test().a();
+    }
+
+    public int a() {
         a = new int[5];
         io.println(a.length);
-
+        return 1;
     }
 }
